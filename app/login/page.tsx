23e6a980181c193621e073c9a19859b7b5a97ab1
Raindrags <raindrags12@gmail.com>
2026-04-23 01:11:36 +0700

    fix user side 1

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,9 +10,9 @@ import {
   CardHeader,
   CardTitle,
 } from "../components/ui/card";
-import { Label } from "../components/ui/label";
-import { Input } from "../components/ui/input";
-import { Button } from "../components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default function LoginPage() {
   const router = useRouter();
